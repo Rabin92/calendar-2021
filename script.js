cf864f9a7e80159abd6d9a5d1918f9b43b967fa0
Rabin Gharti Magar <rabin_mgr@live.com>
2020-12-30 21:19:58 +0000

    files moved to folders

--- script.js
+++
@@ -1,9 +0,0 @@
-const largeScreen = window.matchMedia('(min-width: 768px)');
-const cardContainer = document.querySelector('.grid-container');
-
-if (largeScreen.matches) {
-  cardContainer.classList.add(
-    'animate__animated',
-    'animate__lightSpeedInRight'
-  );
-}
