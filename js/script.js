6df90cb60a1bd1d829438b173939e15beddf28e3
Rabin Gharti Magar <rabin_mgr@live.com>
2020-12-30 21:18:43 +0000

    new folder created

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Variables
+
+const largeScreen = window.matchMedia('(min-width: 768px)');
+const cardContainer = document.querySelector('.grid-container');
+const input = document.querySelector('#js-search');
+const clear = document.querySelector('.search');
+const calendarContent = document.querySelectorAll('.calendar-content');
+
+// Media Query
+
+if (largeScreen.matches) {
+  cardContainer.classList.add(
+    'animate__animated',
+    'animate__lightSpeedInRight'
+  );
+}
+
+// Search Functionality
+
+input.addEventListener('keyup', e => {
+  const theTargetValue = e.target.value.toUpperCase();
+  const headerTitles = document.querySelectorAll('.header__title');
+
+  headerTitles.forEach((headerTitle, i) => {
+    const titleContent = headerTitle.textContent.toUpperCase();
+
+    if (titleContent.includes(theTargetValue)) {
+      calendarContent[i].style.maxWidth = '500px';
+      calendarContent[i].style.display = 'block';
+    } else {
+      calendarContent[i].style.display = 'none';
+    }
+  });
+});
+
+// Clear the value inserted by user
+
+clear.addEventListener('click', e => {
+  let val = input.value;
+
+  for (let i = 0; i < calendarContent.length; i++) {
+    if (val.length > 0) {
+      if (e.target.className === 'cross') {
+        input.value = '';
+        calendarContent[i].style.display = 'block';
+      }
+    }
+  }
+});
